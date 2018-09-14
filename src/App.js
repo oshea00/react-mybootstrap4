19b5fcff3fd6414c9c63ed47f505178aa70b6a04
Mike OShea <oshea00@gmail.com>
2018-09-14 14:45:42 -0700

    cleaner org of bootstrap deps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import './App.css';
 import Game from '../src/components/Game'
+import Navigation from './components/Navigation';
 
 /*
 lodash
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="container">
+      <Navigation/>
       <h1>Play</h1>
       <Game/>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './include/bootstrap'
+import App from '../src/App'
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
